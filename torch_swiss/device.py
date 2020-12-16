172f8f0a553f11fcce5daaa5b714c61e13ad7b26
Philip <p208p2002@gmail.com>
2020-12-16 16:32:58 +0800

    docstring

--- torch_swiss/device.py
+++ torch_swiss/device.py
@@ -2,9 +2,24 @@ import torch
 import torch.nn as nn
 
 def detect_device():
+    """
+    detect cuda available or not
+
+    Returns:
+        class <torch.device>: torch device
+    """
     return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 def auto_apply_device(model):
+    """
+    auto apply model into muilt-gpus
+
+    Args:
+        model (class<torch.nn.Module>): pytorch model
+    
+    Returns:
+        model (class<torch.nn.Module>): pytorch model(into nn.DataParallel)
+    """
     device = detect_device()
     if torch.cuda.device_count() >1:
         model = nn.DataParallel(model,device_ids=range(torch.cuda.device_count()))
--- torch_swiss/model.py
+++ torch_swiss/model.py
@@ -4,10 +4,32 @@ import torch
 import datetime
 
 class ModelHolder():
+    """
+    `ModelHolder` is a model manager.\n
+    You can manual save model by using `self.save_checkpoint`,\n
+    and `ModelHolder` also auto save model while the end of `ModelHolder`.\n
+    All save model will store under `./.model_holder`.\n
+
+    Usage Example:
+        `
+        with ModelHolder(model) as (model_holder,model):            
+        `
+    """
+
     def __init__(self,model):
+        """
+        Args:
+            model (class<torch.nn.Module>): model
+        """
         self.model = model
     
     def save_checkpoint(self,save_name = None):
+        """
+        save checkpoint manual
+
+        Args:
+            save_name (str): name to save, model will store under .model_holder
+        """
         if not os.path.isdir('.model_holder'):
             os.mkdir('.model_holder')
 
@@ -17,9 +39,17 @@ class ModelHolder():
         torch.save(model_to_save, '.model_holder/%s'%save_name)
     
     def __enter__(self):
+        """
+        Returns:
+            self,self.model
+        """
         return self,self.model
     
     def __exit__(self,exc_type, exc_val, exc_tb):
+        """
+        auto save checkpoint while exit ModelHolder
+        """
+
         if exc_type is KeyboardInterrupt:
             print("Catch KeyboardInterrupt, model will auto save in .model_holder")
         
